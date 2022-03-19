be3b7ef09aa8ab68327adb67dca6800b4395fc07
Balparang <shineecard5@gmail.com>
2022-03-20 00:49:15 +0900

    화면(액티비티) 새로만들기, 화면전환(Intent)

--- app/src/main/java/com/preferkim/bts/MainActivity.kt
+++ app/src/main/java/com/preferkim/bts/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.preferkim.bts
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
@@ -15,12 +16,11 @@ class MainActivity : AppCompatActivity() {
         // 1. (프로그램이)화면을 클릭되었다는 것을 알아야함
         val image1 = findViewById<ImageView>(R.id.btsImage_1)
         image1.setOnClickListener { // image1을 클릭하면, "1번 클릭 완료" 메시지를 띄운다
-
             Toast.makeText(this, "1번 클릭 완료", Toast.LENGTH_LONG).show()
 
+            // 2. 화면이 클릭되면, 다음 화면으로 넘어가서, 사진을 크게 보여줌! (메인 액티비티에서 서브 액티비티 실행)
+            val intent = Intent(this, BTS1Activity::class.java) // 다음화면으로 이동하기 위한 인텐트 객체 생성
+            startActivity(intent)
         }
-
-
-        // 2. 화면이 클릭되면, 다음 화면으로 넘어가서, 사진을 크게 보여줌!
     }
 }
