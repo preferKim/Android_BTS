8fc63ebc8bde95200408b4ddc65ec644595e6903
Balparang <shineecard5@gmail.com>
2022-03-20 17:22:06 +0900

    [추가] 여러개의 액티비티 화면전환

--- app/src/main/java/com/preferkim/bts/MainActivity.kt
+++ app/src/main/java/com/preferkim/bts/MainActivity.kt
@@ -22,5 +22,55 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, BTS1Activity::class.java) // 다음화면으로 이동하기 위한 인텐트 객체 생성
             startActivity(intent)
         }
+
+        // 남은 6개의 image들의 id를붙이고, 각각 클릭시, 각각의 액티비티로 이동!
+        val image2 = findViewById<ImageView>(R.id.btsImage_2)
+        val image3 = findViewById<ImageView>(R.id.btsImage_3)
+        val image4 = findViewById<ImageView>(R.id.btsImage_4)
+        val image5 = findViewById<ImageView>(R.id.btsImage_5)
+        val image6 = findViewById<ImageView>(R.id.btsImage_6)
+        val image7 = findViewById<ImageView>(R.id.btsImage_7)
+
+        image2.setOnClickListener {
+            Toast.makeText(this, "2번 클릭 완료", Toast.LENGTH_LONG).show()
+
+            val intent = Intent(this, BTS2Activity::class.java)
+            startActivity(intent)
+        }
+
+        image3.setOnClickListener {
+            Toast.makeText(this, "3번 클릭 완료", Toast.LENGTH_LONG).show()
+
+            val intent = Intent(this, BTS2Activity::class.java)
+            startActivity(intent)
+        }
+
+        image4.setOnClickListener {
+            Toast.makeText(this, "4번 클릭 완료", Toast.LENGTH_LONG).show()
+
+            val intent = Intent(this, BTS2Activity::class.java)
+            startActivity(intent)
+        }
+
+        image5.setOnClickListener {
+            Toast.makeText(this, "5번 클릭 완료", Toast.LENGTH_LONG).show()
+
+            val intent = Intent(this, BTS2Activity::class.java)
+            startActivity(intent)
+        }
+
+        image6.setOnClickListener {
+            Toast.makeText(this, "6번 클릭 완료", Toast.LENGTH_LONG).show()
+
+            val intent = Intent(this, BTS2Activity::class.java)
+            startActivity(intent)
+        }
+
+        image7.setOnClickListener {
+            Toast.makeText(this, "7번 클릭 완료", Toast.LENGTH_LONG).show()
+
+            val intent = Intent(this, BTS2Activity::class.java)
+            startActivity(intent)
+        }
     }
 }
